072163773f782dd700781086ff2bcb410e70ae0f
Lyoung0789 <Lestermyoung07@gmail.com>
2020-07-09 19:28:48 -0400

    Styling Navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Navbar from './components/Navbar'
 import Home from './components/Home'
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
--- src/components/Home.js
+++ src/components/Home.js
@@ -14,28 +14,13 @@ export default class Home extends Component {
                   <div className="overlay" />
                   <div className="container-fluid">
                     <div className="row">
-                      <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                      <div className="col-md-6 col-md-offset-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                         <div className="slider-text-inner js-fullheight">
-                          <div className="desc">
-                            <h1>Hello and Welcome! <br />My name is Lester</h1>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </li>
-                <li style={{backgroundImage: 'url(images/img_bg_2.jpg)'}}>
-                  <div className="overlay" />
-                  <div className="container-fluid">
-                    <div className="row">
-                      <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
-                        <div className="slider-text-inner">
-                          <div className="desc">
-                            <h1>I am <br />a full-stack web developer</h1>
-                            <h2>100% html5 bootstrap templates Made by <a href="https://colorlib.com/" target="_blank">colorlib.com</a></h2>
-                            <div class="arrow bounce">
-		                        <a class="fa fa-arrow-down fa-2x" href="#">enter</a>
-	                        </div>
+                          <div className="desc" >
+                            <h1>Hello and Welcome! <br />My name is Lester <br/> And I am a Full Stack Web developer</h1>
+                            <div className="arrow bounce">
+		                        <a className="fa fa-arrow-down fa-2x" href="#">enter</a>
+                            </div>
                           </div>
                         </div>
                       </div>

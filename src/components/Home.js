c7f3b8421e5f04c3e773f729e3d0610d62f30095
Lyoung0789 <Lestermyoung07@gmail.com>
2020-07-10 16:24:06 -0400

    adding some pictures

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,14 +10,14 @@ export default class Home extends Component {
           <section id="colorlib-hero" className="js-fullheight" data-section="home">
             <div className="flexslider js-fullheight">
               <ul className="slides">
-                <li style={{backgroundImage: 'url(images/img_bg_1.jpg)'}}>
+                <li style={{backgroundImage: 'url(images/home.jpeg)'}}>
                   <div className="overlay" />
                   <div className="container-fluid">
                     <div className="row">
                       <div className="col-md-6 col-md-offset-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                         <div className="slider-text-inner js-fullheight">
                           <div className="desc" >
-                            <h1>Hello and Welcome! <br />My name is Lester <br/> And I am a Full Stack Web developer</h1>
+                            <h1 style={{color:"white"}}>Hello and Welcome! <br />My name is Lester <br/> And I am a Full Stack Web developer</h1>
                             <div className="arrow bounce">
 		                        <a className="fa fa-arrow-down fa-2x" href="#">enter</a>
                             </div>

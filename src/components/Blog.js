a4ba752b45749a753e6425404f60be415c5053b2
Lyoung0789 <Lestermyoung07@gmail.com>
2020-07-12 12:20:49 -0400

    adding color scheme and links

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -17,30 +17,30 @@ export default class Blog extends Component{
                         <div className="row">
                           <div className="col-md-4 col-sm-6 animate-box" data-animate-effect="fadeInLeft">
                             <div className="blog-entry">
-                              <a href="blog.html" className="blog-img"><img src="images/blog-1.jpg" className="img-responsive" alt="HTML5 Bootstrap Template by colorlib.com" /></a>
+                              <a href="https://lyoung0789.github.io/react_redux" className="blog-img"><img src="https://images.unsplash.com/photo-1581276879432-15e50529f34b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80" className="img-responsive" alt="HTML5 Bootstrap Template by colorlib.com" /></a>
                               <div className="desc">
                                 <span><small>July 5, 2020 </small> | <small>JavaScript</small> </span>
-                                <h3><a href="blog.html">React Redux</a></h3>
+                                <h3><a href="https://lyoung0789.github.io/react_redux">React Redux</a></h3>
                                 <p>We’re here! The Final Project. It feels like it was yesterday, working on my first CLI project, but look how far we’ve come! For my Final Project I decided to create an Application that keeps track of users goals. The user can log what their goals are and maintain a checklist to Reach those goals. Hence, I have named it Reach.</p>
                               </div>
                             </div>
                           </div>
                           <div className="col-md-4 col-sm-6 animate-box" data-animate-effect="fadeInRight">
                             <div className="blog-entry">
-                              <a href="blog.html" className="blog-img"><img src="images/blog-2.jpg" className="img-responsive" alt="HTML5 Bootstrap Template by colorlib.com" /></a>
+                              <a href="https://lyoung0789.github.io/welcome_to_javascript" className="blog-img"><img src="https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80" className="img-responsive" alt="HTML5 Bootstrap Template by colorlib.com" /></a>
                               <div className="desc">
                                 <span><small>June 6, 2020</small> | <small> JavaScript</small></span>
-                                <h3><a href="blog.html">Welcome to JavaScript</a></h3>
+                                <h3><a href="https://lyoung0789.github.io/welcome_to_javascript">Welcome to JavaScript</a></h3>
                                 <p>“Welcome to JavaScript”. The words that have brought me so much joy and so much pain. For my fourth project I developed a Home Bartender application that lets users share their favorite cocktails. Users, are able to comment, like, and add their own recipes to share with other aspiring at home bartenders. </p>
                               </div>
                             </div>
                           </div>
                           <div className="col-md-4 col-sm-6 animate-box" data-animate-effect="fadeInLeft">
                             <div className="blog-entry">
-                              <a href="blog.html" className="blog-img"><img src="images/blog-3.jpg" className="img-responsive" alt="HTML5 Bootstrap Template by colorlib.com" /></a>
+                              <a href="https://lyoung0789.github.io/rails_portfolio_project" className="blog-img"><img src="https://images.unsplash.com/photo-1558415652-660b27fd1878?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2167&q=80" className="img-responsive" alt="HTML5 Bootstrap Template by colorlib.com" /></a>
                               <div className="desc">
                                 <span><small>May 10, 2020 </small> | <small> Ruby </small></span>
-                                <h3><a href="blog.html">Rails Portfolio Project</a></h3>
+                                <h3><a href="https://lyoung0789.github.io/rails_portfolio_project">Rails Portfolio Project</a></h3>
                                 <p>Project 3! Okay here we go, for my third portfolio project I created a Handyman application in which users are able to book appointments with technicians that are signed up for the application. Users can also be managers and create Technicians and view their appointments.</p>
                               </div>
                             </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -10,14 +10,14 @@ export default class Home extends Component {
           <section id="colorlib-hero" className="js-fullheight" data-section="home">
             <div className="flexslider js-fullheight">
               <ul className="slides">
-                <li style={{backgroundImage: 'url(images/home.jpeg)'}}>
+                <li style={{backgroundImage: 'url(https://images.unsplash.com/photo-1518655048521-f130df041f66?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80)'}}>
                   <div className="overlay" />
                   <div className="container-fluid">
                     <div className="row">
                       <div className="col-md-6 col-md-offset-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                         <div className="slider-text-inner js-fullheight">
                           <div className="desc" >
-                            <h1 style={{color:"white"}}>Hello and Welcome! <br />My name is Lester <br/> And I am a Full Stack Web developer</h1>
+                            <h1 style={{color:"black"}}>Hello and Welcome! <br />My name is Lester <br/> And I am a Full Stack Web developer</h1>
                           </div>
                         </div>
                       </div>

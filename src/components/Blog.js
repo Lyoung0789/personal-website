24727c463cab991d4890c0934ef581f960b2bd50
Lyoung0789 <Lestermyoung07@gmail.com>
2020-09-17 21:49:43 -0400

    updated with new blog

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,41 +1,127 @@
-import React, {Component} from 'react'
+import React, { Component } from "react";
 
-export default class Blog extends Component{
-
-    render(){
-        return(
-
-            <div className="col-md-offset-2">
-                    <section className="colorlib-blog " data-section="blog">
-                      <div className="colorlib-narrow-content">
-                        <div className="row">
-                          <div className=" animate-box" data-animate-effect="fadeInLeft">
-                            <span className="heading-meta">Read</span>
-                            <h2 className="colorlib-heading">Recent Blog</h2>
-                          </div>
-                        </div>
-                        <div className="row">
-                          <div className="col-md-4 col-sm-6 animate-box" data-animate-effect="fadeInLeft">
-                            <div className="blog-entry">
-                              <a href="https://lyoung0789.github.io/an_end_to_a_chapter" className="blog-img"><img src="https://images.unsplash.com/photo-1544776943-031655f13833?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2242&q=80" className="img-responsive" alt="HTML5 Bootstrap Template by colorlib.com" /></a>
-                              <div className="desc">
-                                <span><small>July 16, 2020 </small> | <small> Ruby </small></span>
-                                <h3><a href="https://lyoung0789.github.io/an_end_to_a_chapter">An End to a Chapter</a></h3>
-                                <p>It feels like it was just yesterday, when I first started FlatIron School for Software Engineering, and now it’s almost over! This experience has been amazing! I learned so much over the course of 5 months and to be honest, I’m kinda sad that it’s over. I had the absolute best experience, here and I would recommend this program to anyone that wants to learn coding!</p>
-                              </div>
-                            </div>
-                          </div>
-                          <div className="col-md-4 col-sm-6 animate-box" data-animate-effect="fadeInLeft">
-                            <div className="blog-entry">
-                              <a href="https://lyoung0789.github.io/react_redux" className="blog-img"><img src="https://images.unsplash.com/photo-1581276879432-15e50529f34b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80" className="img-responsive" alt="HTML5 Bootstrap Template by colorlib.com" /></a>
-                              <div className="desc">
-                                <span><small>July 5, 2020 </small> | <small>JavaScript</small> </span>
-                                <h3><a href="https://lyoung0789.github.io/react_redux">React Redux</a></h3>
-                                <p>We’re here! The Final Project. It feels like it was yesterday, working on my first CLI project, but look how far we’ve come! For my Final Project I decided to create an Application that keeps track of users goals. The user can log what their goals are and maintain a checklist to Reach those goals. Hence, I have named it Reach.</p>
-                              </div>
-                            </div>
-                          </div>
-                          <div className="col-md-4 col-sm-6 animate-box" data-animate-effect="fadeInRight">
+export default class Blog extends Component {
+  render() {
+    return (
+      <div className="col-md-offset-2">
+        <section className="colorlib-blog " data-section="blog">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div className=" animate-box" data-animate-effect="fadeInLeft">
+                <span className="heading-meta">Read</span>
+                <h2 className="colorlib-heading">Recent Blog</h2>
+              </div>
+            </div>
+            <div className="row">
+              <div
+                className="col-md-4 col-sm-6 animate-box"
+                data-animate-effect="fadeInLeft"
+              >
+                <div className="blog-entry">
+                  <a
+                    href="https://lyoung0789.github.io/react_native_intro"
+                    className="blog-img"
+                  >
+                    <img
+                      src="https://www.manhattanmobile.com/wp-content/uploads/2018/08/react-native-workshop.jpg"
+                      className="img-responsive"
+                      alt="HTML5 Bootstrap Template by colorlib.com"
+                      style={{ height: 290 }}
+                    />
+                  </a>
+                  <div className="desc">
+                    <span>
+                      <small>September 17, 2020 </small> |{" "}
+                      <small> React Native</small>
+                    </span>
+                    <h3>
+                      <a href="https://lyoung0789.github.io/react_native_intro">
+                        React Native Intro
+                      </a>
+                    </h3>
+                    <p>
+                      Hello again! After learning React, I wanted to get into
+                      building mobile applications. And since I just learned
+                      React, React Native was an obvious choice. In this blog
+                      post I will be briefly introducing React Native and
+                      setting up a simple application called TodoApp from
+                      scratch using React Native. Let’s get started!
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div
+                className="col-md-4 col-sm-6 animate-box"
+                data-animate-effect="fadeInLeft"
+              >
+                <div className="blog-entry">
+                  <a
+                    href="https://lyoung0789.github.io/an_end_to_a_chapter"
+                    className="blog-img"
+                  >
+                    <img
+                      src="https://images.unsplash.com/photo-1544776943-031655f13833?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2242&q=80"
+                      className="img-responsive"
+                      alt="HTML5 Bootstrap Template by colorlib.com"
+                    />
+                  </a>
+                  <div className="desc">
+                    <span>
+                      <small>July 16, 2020 </small> | <small> Ruby </small>
+                    </span>
+                    <h3>
+                      <a href="https://lyoung0789.github.io/an_end_to_a_chapter">
+                        An End to a Chapter
+                      </a>
+                    </h3>
+                    <p>
+                      It feels like it was just yesterday, when I first started
+                      FlatIron School for Software Engineering, and now it’s
+                      almost over! This experience has been amazing! I learned
+                      so much over the course of 5 months and to be honest, I’m
+                      kinda sad that it’s over. I had the absolute best
+                      experience, here and I would recommend this program to
+                      anyone that wants to learn coding!
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div
+                className="col-md-4 col-sm-6 animate-box"
+                data-animate-effect="fadeInLeft"
+              >
+                <div className="blog-entry">
+                  <a
+                    href="https://lyoung0789.github.io/react_redux"
+                    className="blog-img"
+                  >
+                    <img
+                      src="https://images.unsplash.com/photo-1581276879432-15e50529f34b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80"
+                      className="img-responsive"
+                      alt="HTML5 Bootstrap Template by colorlib.com"
+                    />
+                  </a>
+                  <div className="desc">
+                    <span>
+                      <small>July 5, 2020 </small> | <small>JavaScript</small>{" "}
+                    </span>
+                    <h3>
+                      <a href="https://lyoung0789.github.io/react_redux">
+                        React Redux
+                      </a>
+                    </h3>
+                    <p>
+                      We’re here! The Final Project. It feels like it was
+                      yesterday, working on my first CLI project, but look how
+                      far we’ve come! For my Final Project I decided to create
+                      an Application that keeps track of users goals. The user
+                      can log what their goals are and maintain a checklist to
+                      Reach those goals. Hence, I have named it Reach.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              {/* <div className="col-md-4 col-sm-6 animate-box" data-animate-effect="fadeInRight">
                             <div className="blog-entry">
                               <a href="https://lyoung0789.github.io/welcome_to_javascript" className="blog-img"><img src="https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80" className="img-responsive" alt="HTML5 Bootstrap Template by colorlib.com" /></a>
                               <div className="desc">
@@ -44,8 +130,8 @@ export default class Blog extends Component{
                                 <p>“Welcome to JavaScript”. The words that have brought me so much joy and so much pain. For my fourth project I developed a Home Bartender application that lets users share their favorite cocktails. Users, are able to comment, like, and add their own recipes to share with other aspiring at home bartenders. </p>
                               </div>
                             </div>
-                          </div>
-                          {/* <div className="col-md-4 col-sm-6 animate-box" data-animate-effect="fadeInLeft">
+                          </div> */}
+              {/* <div className="col-md-4 col-sm-6 animate-box" data-animate-effect="fadeInLeft">
                             <div className="blog-entry">
                               <a href="https://lyoung0789.github.io/rails_portfolio_project" className="blog-img"><img src="https://images.unsplash.com/photo-1544776943-031655f13833?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2242&q=80" className="img-responsive" alt="HTML5 Bootstrap Template by colorlib.com" /></a>
                               <div className="desc">
@@ -55,16 +141,22 @@ export default class Blog extends Component{
                               </div>
                             </div>
                           </div> */}
-                        </div>
-                        <div className="row">
-                          <div className="col-md-12 animate-box">
-                            <p><a href="https://lyoung0789.github.io/" className="btn btn-primary btn-lg btn-load-more">View Blog </a></p>
-                          </div>
-                        </div>
-                      </div>
-                    </section>
-                </div>
-          
-        )
-    }
+            </div>
+            <div className="row">
+              <div className="col-md-12 animate-box">
+                <p>
+                  <a
+                    href="https://lyoung0789.github.io/"
+                    className="btn btn-primary btn-lg btn-load-more"
+                  >
+                    View Blog{" "}
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    );
+  }
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,83 +1,170 @@
-import React, {Component} from 'react'
+import React, { Component } from "react";
 
 export default class Projects extends Component {
-
-    render(){
-        return(
-            <div className="col-md-offset-2">
-                
-                    <section className="colorlib-work" data-section="work">
-                      <div className="colorlib-narrow-content col-md-12">
-                        <div className="row">
-                          <div className="animate-box" data-animate-effect="fadeInLeft">
-                            <span className="heading-meta">My Projects</span>
-                            <h2 className="colorlib-heading animate-box">Recent Work</h2>
-                          </div>
-                        </div>
-                        {/* <div className="row row-bottom-padded-sm animate-box" data-animate-effect="fadeInLeft">
+  render() {
+    return (
+      <div className="col-md-offset-2">
+        <section className="colorlib-work" data-section="work">
+          <div className="colorlib-narrow-content col-md-12">
+            <div className="row">
+              <div className="animate-box" data-animate-effect="fadeInLeft">
+                <span className="heading-meta">My Projects</span>
+                <h2 className="colorlib-heading animate-box">Recent Work</h2>
+              </div>
+            </div>
+            {/* <div className="row row-bottom-padded-sm animate-box" data-animate-effect="fadeInLeft">
                           <div className="col-md-12">
                             <p className="work-menu"><span><a href="google.com" className="active">Graphic Design</a></span> <span><a href="google.com">Web Design</a></span> <span><a href="google.com">Software</a></span> <span><a href="google.com">Apps</a></span></p>
                           </div>
                         </div> */}
-                        <div className="row">
-                          <div className="col-md-6 animate-box" data-animate-effect="fadeInLeft">
-                            <div className="project" style={{backgroundImage: 'url(https://images.unsplash.com/photo-1492176273113-2d51f47b23b0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80)'}}>
-                              <div className="desc">
-                                <div className="con">
-                                  <h3><a href="work.html">Reach</a></h3>
-                                  <span><p>An app developed to track goals along with pending action items, featuring a checklist to help you achieve your targets.</p></span>
-                                  <p className="icon">
-                                    <span><a href="https://github.com/Lyoung0789/reach-frontend"><i className="icon-git" /></a></span>
-                                    <span><a href="https://www.youtube.com/watch?v=02UWWDOHFns"><i className="icon-youtube" /></a></span>
-                                  </p>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                          <div className="col-md-6 animate-box" data-animate-effect="fadeInRight">
-                            <div className="project" style={{backgroundImage: 'url(https://images.unsplash.com/photo-1542600176-4f7f2cba84d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80)'}}>
-                              <div className="desc">
-                                <div className="con">
-                                  <h3><a href="work.html">HomeBartender</a></h3>
-                                  <span><p>An app created to share your own twist on cocktail recipes with enabled ratings and comments.</p></span>
-                                  <p className="icon">
-                                    <span><a href="https://github.com/Lyoung0789/bartender-frontend.git"><i className="icon-git" /></a></span>
-                                    <span><a href="https://www.youtube.com/watch?v=E3gJWqiFrcI&t=1s"><i className="icon-youtube" /></a></span>
-                                    
-                                  </p>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                          <div className="col-md-6 animate-box" data-animate-effect="fadeInTop">
-                            <div className="project" style={{backgroundImage: 'url(https://images.unsplash.com/reserve/oIpwxeeSPy1cnwYpqJ1w_Dufer%20Collateral%20test.jpg?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1832&q=80)'}}>
-                              <div className="desc">
-                                <div className="con">
-                                  <h3><a href="work.html">HandyMan</a></h3>
-                                  <span><p>An app designed for users to sign in, book and request appointments with local technicians (ie: AC, Plumber, Electric).</p></span>
-                                  <p className="icon">
-                                    <span><a href="https://github.com/Lyoung0789/handyman-finder.git"><i className="icon-git" /></a></span>
-                                    <span><a href="https://www.youtube.com/watch?v=5AGmffKrvUI&t=292s"><i className="icon-youtube" /></a></span>
-                                  </p>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                          <div className="col-md-6 animate-box" data-animate-effect="fadeInBottom">
-                            <div className="project" style={{backgroundImage: 'url(https://images.unsplash.com/photo-1548529793-1325ac6b99a6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80)'}}>
-                              <div className="desc">
-                                <div className="con">
-                                  <h3><a href="work.html">HappyHour</a></h3>
-                                  <span>A CLI application that allows you to find 10 happy hour locations, that is within 25 miles of your zipcode. The application provides the address, hours and specials of each location.</span>
-                                  <p className="icon">
-                                    <span><a href="https://github.com/Lyoung0789/happy_hour.git"><i className="icon-git" /></a></span>
-                                    <span><a href="https://www.youtube.com/watch?v=DckfgGudKSk&t=12s"><i className="icon-youtube" /></a></span>
-                                  </p>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                          {/* <div className="col-md-6 animate-box" data-animate-effect="fadeInLeft">
+            <div className="row">
+              <div
+                className="col-md-6 animate-box"
+                data-animate-effect="fadeInLeft"
+              >
+                <div
+                  className="project"
+                  style={{
+                    backgroundImage:
+                      "url(https://images.unsplash.com/photo-1492176273113-2d51f47b23b0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80)",
+                  }}
+                >
+                  <div className="desc">
+                    <div className="con">
+                      <h3 style={{ color: "white" }}>Reach</h3>
+                      <span>
+                        <p>
+                          An app developed to track goals along with pending
+                          action items, featuring a checklist to help you
+                          achieve your targets.
+                        </p>
+                      </span>
+                      <p className="icon">
+                        <span>
+                          <a href="https://github.com/Lyoung0789/reach-frontend">
+                            <i className="icon-git" />
+                          </a>
+                        </span>
+                        <span>
+                          <a href="https://www.youtube.com/watch?v=02UWWDOHFns">
+                            <i className="icon-youtube" />
+                          </a>
+                        </span>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div
+                className="col-md-6 animate-box"
+                data-animate-effect="fadeInRight"
+              >
+                <div
+                  className="project"
+                  style={{
+                    backgroundImage:
+                      "url(https://images.unsplash.com/photo-1542600176-4f7f2cba84d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80)",
+                  }}
+                >
+                  <div className="desc">
+                    <div className="con">
+                      <h3 style={{ color: "white" }}>HomeBartender</h3>
+                      <span>
+                        <p>
+                          An app created to share your own twist on cocktail
+                          recipes with enabled ratings and comments.
+                        </p>
+                      </span>
+                      <p className="icon">
+                        <span>
+                          <a href="https://github.com/Lyoung0789/bartender-frontend.git">
+                            <i className="icon-git" />
+                          </a>
+                        </span>
+                        <span>
+                          <a href="https://www.youtube.com/watch?v=E3gJWqiFrcI&t=1s">
+                            <i className="icon-youtube" />
+                          </a>
+                        </span>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div
+                className="col-md-6 animate-box"
+                data-animate-effect="fadeInTop"
+              >
+                <div
+                  className="project"
+                  style={{
+                    backgroundImage:
+                      "url(https://images.unsplash.com/reserve/oIpwxeeSPy1cnwYpqJ1w_Dufer%20Collateral%20test.jpg?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1832&q=80)",
+                  }}
+                >
+                  <div className="desc">
+                    <div className="con">
+                      <h3 style={{ color: "white" }}>HandyMan</h3>
+                      <span>
+                        <p>
+                          An app designed for users to sign in, book and request
+                          appointments with local technicians (ie: AC, Plumber,
+                          Electric).
+                        </p>
+                      </span>
+                      <p className="icon">
+                        <span>
+                          <a href="https://github.com/Lyoung0789/handyman-finder.git">
+                            <i className="icon-git" />
+                          </a>
+                        </span>
+                        <span>
+                          <a href="https://www.youtube.com/watch?v=5AGmffKrvUI&t=292s">
+                            <i className="icon-youtube" />
+                          </a>
+                        </span>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div
+                className="col-md-6 animate-box"
+                data-animate-effect="fadeInBottom"
+              >
+                <div
+                  className="project"
+                  style={{
+                    backgroundImage:
+                      "url(https://images.unsplash.com/photo-1548529793-1325ac6b99a6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80)",
+                  }}
+                >
+                  <div className="desc">
+                    <div className="con">
+                      <h3 style={{ color: "white" }}>HappyHour</h3>
+                      <span>
+                        A CLI application that allows you to find 10 happy hour
+                        locations, that is within 25 miles of your zipcode. The
+                        application provides the address, hours and specials of
+                        each location.
+                      </span>
+                      <p className="icon">
+                        <span>
+                          <a href="https://github.com/Lyoung0789/happy_hour.git">
+                            <i className="icon-git" />
+                          </a>
+                        </span>
+                        <span>
+                          <a href="https://www.youtube.com/watch?v=DckfgGudKSk&t=12s">
+                            <i className="icon-youtube" />
+                          </a>
+                        </span>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {/* <div className="col-md-6 animate-box" data-animate-effect="fadeInLeft">
                             <div className="project" style={{backgroundImage: 'url(images/img-5.jpg)'}}>
                               <div className="desc">
                                 <div className="con">
@@ -92,7 +179,7 @@ export default class Projects extends Component {
                               </div>
                             </div>
                           </div> */}
-                          {/* <div className="col-md-6 animate-box" data-animate-effect="fadeInRight">
+              {/* <div className="col-md-6 animate-box" data-animate-effect="fadeInRight">
                             <div className="project" style={{backgroundImage: 'url(images/img-6.jpg)'}}>
                               <div className="desc">
                                 <div className="con">
@@ -107,16 +194,15 @@ export default class Projects extends Component {
                               </div>
                             </div>
                           </div> */}
-                        </div>
-                        {/* <div className="row">
+            </div>
+            {/* <div className="row">
                           <div className="col-md-12 animate-box">
                             <p><a href="#" className="btn btn-primary btn-lg btn-load-more">Load more <i className="icon-reload" /></a></p>
                           </div>
                         </div> */}
-                      </div>
-                    </section>
-                    </div>
-
-        )
-    }
+          </div>
+        </section>
+      </div>
+    );
+  }
 }
